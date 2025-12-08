2abb24029c2a6812d1dfaf2704b111857d612330
boukhrisadem <boukhris.adem99@gmail.com>
2025-12-08 23:32:30 +0100

    Remove secrets from version control, add example config

---
+++ backend/recipe-service/src/main/java/com/scan_n_eat/recipe_service/RecipeServiceApplication.java
@@ -0,0 +1,12 @@
+package com.scan_n_eat.recipe_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeServiceApplication.class, args);
+    }
+}
