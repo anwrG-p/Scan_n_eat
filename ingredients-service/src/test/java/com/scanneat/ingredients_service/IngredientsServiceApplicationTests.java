0b64797be6118c7c0a891e8f6a69a212ae4c7a6b
Dhafer <mohamedaminedhafer@gmail.com>
2025-12-08 23:44:23 +0100

    initialization

---
+++ ingredients-service/src/test/java/com/scanneat/ingredients_service/IngredientsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scanneat.ingredients_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngredientsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
